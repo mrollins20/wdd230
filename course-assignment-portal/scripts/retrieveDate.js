b9b6b5807f663ae135f32ecd7a257740680f3168
mrollins20 <crazydude874@gmail.com>
2022-09-17 23:44:53 -0700

    Added course assignment portal.

---
+++ course-assignment-portal/scripts/retrieveDate.js
@@ -0,0 +1,21 @@
+// Fetches the current date and time.
+const currentDateAndTime = new Date();
+
+// Retrieve the year from the current date.
+let copyrightYear = currentDateAndTime.getFullYear();
+
+//Retrieve the last date that the webpage was modified and convert it into parts.
+let modificationDate = new Date(document.lastModified);
+let modificationYear = modificationDate.getFullYear();
+let modificationMonth = modificationDate.getMonth() + 1; //1 is added to the month value due to January's index being 0.
+let modificationDay = modificationDate.getDate();
+let modificationHours = modificationDate.getHours();
+let modificationSeconds = modificationDate.getSeconds();
+let modificationMiliseconds = modificationDate.getMilliseconds();
+
+//Return the copyright year and Modification date.
+document.getElementById("copyright-year").textContent = `${copyrightYear}`;
+
+document.getElementById("last-modified-date").textContent = 
+    `Last Updated: ${modificationMonth}/${modificationDay}/${modificationYear} 
+    ${modificationHours}:${modificationSeconds}:${modificationMiliseconds}`;
