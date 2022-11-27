30ed199ff1f13208c2109b654e1630ce808bbc9b
mrollins20 <crazydude874@gmail.com>
2022-11-26 17:00:00 -0700

    removed unnecessary files/script

--- chamber/scripts/feedback.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".submit-button").addEventListener("click", toggleMessage())
-const message = document.body.querySelector(".feedback-msg");
-setTimeout().toggleMessage();
-function toggleMessage() {
-    message.toggleAttribute(".feedback-msg-confirmation");
-}
